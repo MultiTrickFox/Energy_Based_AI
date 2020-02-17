10ab1ec94d3f0855baec1fe0a87234bf3054fbf7
Developer's Fox <39859227+developersfox@users.noreply.github.com>
2020-02-17 15:42:31 +0200

    minor change

--- rbm_sync/test.jl
+++ rbm_sync/test.jl
@@ -46,67 +46,36 @@ test_basic() = begin
 end ; #test_basic()
 
 
-test_train() = begin
-
-
-    rbm = RBM(in_size,10)
-
-    random_state = randn(1,length(rbm.hiddens))
-
-    binary ? random_state = binarize_state.(random_state) : ()
-
-    gen1 = generate(rbm,hidden_state=random_state)
-
-    rbm, meta = train(rbm=rbm,hm_epochs=1)
-
-    gen2 = generate(rbm,hidden_state=random_state)
-
-
-    println(" ")
-
-    gen1 == gen2 ? (println("same.")) : (println("smt changed."))
-    ctr = 0
-    for (e1,e2) in zip(gen1,gen2)
-        e1 != e2 ? ctr +=1 : ()
-    end
-    println("diff ratio: $(ctr/in_size)")
-
-
-    # using ImageView: imshow
-    #
-    # imshow(gen2)
-
-
-end ; #test_train()
-
-
 test_hiddensizes() = begin
 
     hm_avg = 100
 
     hidden_sizes = 1:12
 
-    results = [.0 for _ in hidden_sizes]
+    results = [0.0 for _ in hidden_sizes]
 
-    for _ in 1:hm_avg
+    for ii in 1:hm_avg
 
         for (i,hs) in enum(hidden_sizes)
 
-            results[i] += train(hidden_size=hs,hm_epochs=1)[end][end][end]
+            results[i] = (results[i] * (ii-1) + train(hidden_size=hs,hm_epochs=1)[end][end][end]) / ii
 
         end
 
+        println("iteration: $(ii), lowest loss: $(hidden_sizes[argmin(results)])")
+
     end
 
-    results = [e/hm_avg for e in results]
+    # results = [e/hm_avg for e in results]
 
-    # for (hs,res) in zip(hidden_sizes, results)
-    #
-    #     println("$(hs): $(res)")
-    #
-    # end
+    println("Final Results:")
 
-results
+    for (hs,res) in zip(hidden_sizes, results)
+
+        println("\t$(hs): $(res)")
+
+    end
+
 end ; #test_hiddensizes()
 
 
